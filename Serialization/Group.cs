e60ca92a0c055a269a647f524e8223b13b754842
Sergey Grigoriev <sergey00475@gmail.com>
2020-04-20 11:02:34 +0400

    Добавьте файлы проекта.

---
+++ Serialization/Group.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Group
+    {
+        [NonSerialized]
+        private readonly Random rnd = new Random(DateTime.Now.Millisecond);
+        public int Number { get; set; }
+        public string Name { get; set; }
+
+        public Group() 
+        {
+            Number = rnd.Next(1, 10);
+            Name = "Группа " + rnd;
+        }
+
+        public Group(int number, string name)
+        {
+            // Проверка входних данных
+            Number = number;
+            Name = name;
+
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Serialization/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; }
+        public int  Age { get; }
+
+        public Group Group { get; set; }
+
+        public Student(string name, int age)
+        {
+            // Проверка входных парамитров
+
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
